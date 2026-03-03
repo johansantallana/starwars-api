d675058856e5a67c383ee2a8479f2069b144d82e
Johan Santallana <johan921229@gmail.com>
2026-03-03 17:49:50 -0500

    Clean up formatting in models.py
    Removed unnecessary newline in models.py

--- src/models.py
+++ src/models.py
@@ -3,7 +3,6 @@ from sqlalchemy import String, Boolean, Integer, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
-
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
