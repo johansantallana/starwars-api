8c2bb34abe00bb000f70d41e5294969f26b8b22e
Johan Santallana <johan921229@gmail.com>
2026-03-18 21:32:59 -0400

    Fix formatting and add Person model definition

--- src/models.py
+++ src/models.py
@@ -16,6 +16,7 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+        
 
 class Person(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
